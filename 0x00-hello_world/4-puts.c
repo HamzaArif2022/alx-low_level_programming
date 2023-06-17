7f17ea87d0915b2e75796e79cc5ad58cd6c14107
hamza <hamza@2003.com>
2023-06-17 11:07:41 +0100

    added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,9 +2,8 @@
 
 /**
    * main - Entry point
-    *
-     * Return: 0 on success
-      */
+   * Return: 0 on success
+   */
 int main(void)
 {
 	    puts("\"Programming is like building a multilingual puzzle");
