7e41039ac5b2170a7fe3a8b2d2a71d361d58bc2f
Hamza <Hamza@arif.com>
2023-06-21 16:10:00 -0700

    0x02. C - Functions, nested loops

---
+++ 0x02-functions_nested_loops/8-main.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    jack_bauer();
+    return (0);
+}
+julien@ubuntu:~/0x02$ gcc -Wall -pedantic -Werror -Wextra -std=gnu89 _putchar.c 8-main.c 8-24_hours.c -o 8-24
+julien@ubuntu:~/0x02$ ./8-24 | head
+00:00
+00:01
+00:02
+00:03
+00:04
+00:05
+00:06
+00:07
+00:08
+00:09
+julien@ubuntu:~/0x02$ ./8-24 | tail
+23:50
+23:51
+23:52
+23:53
+23:54
+23:55
+23:56
+23:57
+23:58
+23:59
